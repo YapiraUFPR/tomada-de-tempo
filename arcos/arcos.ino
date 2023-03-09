44e372118cc0df81f1c27cfa5277977caa219322
Gabp0 <gabriel.pontarolo045@gmail.com>
2023-03-09 11:28:14 -0300

    bug fixes

--- arcos/arcos.ino
+++ arcos/arcos.ino
@@ -10,11 +10,11 @@ const int sl_ur_epin = 5;
 // finish line sensors pins
 const int fl_ul_tpin = 6;
 const int fl_ul_epin = 7;
-const int fl_ur_tpin = 8;
-const int fl_ur_epin = 9;
+const int fl_ur_tpin = 9;
+const int fl_ur_epin = 10;
 
 // reset button pin
-const int rst_btn_pin = 10;
+const int rst_btn_pin = 13;
 
 // lcd pins
 const int rs = 11, en = 12, d4 = 13, d5 = 14, d6 = 15, d7 = 16;
@@ -24,6 +24,14 @@ const int US_THRSH = 50;
 
 // code state
 enum LINE_STATE {INIT, START, COUNTING, FINISH};
+
+//initialisation class HCSR04 (trig pin , echo pin)
+HCSR04 sl_ur(sl_ur_tpin, sl_ur_epin);
+HCSR04 sl_ul(sl_ul_tpin, sl_ul_epin); 
+HCSR04 fl_ur(fl_ur_tpin, fl_ur_epin);
+HCSR04 fl_ul(fl_ul_tpin, fl_ul_epin);
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+LINE_STATE state = INIT;
 
 unsigned int start_timestamp = 0;
 unsigned int time_elapsed = 0;
@@ -99,26 +107,31 @@ LINE_STATE finishTrs(bool rst_btn_state)
     return FINISH;
 }
 
-void setup() {
-    //initialisation class HCSR04 (trig pin , echo pin)
-    HCSR04 sl_ur(sl_ur_tpin, sl_ur_epin);
-    HCSR04 sl_ul(sl_ul_tpin, sl_ul_epin); 
-    HCSR04 fl_ur(fl_ur_tpin, fl_ur_epin);
-    HCSR04 fl_ul(fl_ul_tpin, fl_ul_epin);
+bool checkBtn(void)
+{
+    bool btn_state = digitalRead(rst_btn_pin);
+    if (btn_state == HIGH)
+    {
+        while(digitalRead(rst_btn_pin) == HIGH);
+        return true;
+    }
+    return false;
+}
 
+bool checkArc(float right_dist, float left_dist)
+{
+    return (((right_dist != 0) &&  (right_dist < US_THRSH)) || ((left_dist != 0) && (left_dist < US_THRSH)));
+}
+
+void setup() {
     pinMode(rst_btn_pin, INPUT);
 
-    LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
     lcd.begin(16, 2);
-    
-    LINE_STATE state = INIT;
 }
 
 void loop() {
 
-    bool rst_btn_state = digitalRead(rst_btn_pin);
-    bool start_line_state =  (sl_ul.dist() < US_THRSH) || (sl_ur.dist() < US_THRSH);
-    bool finish_line_state =  (fl_ul.dist() < US_THRSH) || (fl_ur.dist() < US_THRSH);
+    bool rst_btn_state = checkBtn();
 
     switch (state)
     {
@@ -127,10 +140,12 @@ void loop() {
             break;
 
         case START:
+            bool start_line_state =  checkArc(sl_ur.dist(), sl_ul.dist());
             state = startTrs(rst_btn_state, start_line_state);    
             break;
 
         case COUNTING:
+            bool finish_line_state =  checkArc(fl_ur.dist(), fl_ul.dist());
             state = countTrs(finish_line_state, rst_btn_state);
             break;
 
